39d09e4c47aa90461e437e7cf1e977eb90b2e723
Ryan Savitski <ryansavitski@gmail.com>
2013-02-13 13:12:57 -0800

    Unsure about normal calculations, working on adding lighting and shading

--- cgRender.cpp
+++ cgRender.cpp
@@ -87,6 +87,9 @@ int main(int argc, char** argv)
 	init();
 
 	//TODO: TEMP
+/*
+	printf("==========MAIN DEBUG===========\n\n");
+
 	GLfloat barbl[3] = {1,2,3};
 	GLfloat vuhdl[3] = {2,3,4};
 	GLfloat res[3] = {0,0,0};
@@ -103,7 +106,8 @@ int main(int argc, char** argv)
 	triangleNormal_unnormalised(pt1,pt2,pt3,res2);
 	printf("%f %f %f\n", res2[0], res2[1], res2[2]);
 	 
-
+	printf("==========MAIN DEBUG===========\n\n");
+*/
 	// Attach callbacks
 	glutDisplayFunc(display); 
 	glutReshapeFunc(reshape);
@@ -144,7 +148,7 @@ void init()
 	facelist = glGenLists(1);
 
 	// compile the display list
-	glNewList(facelist, GL_COMPILE);
+	/*glNewList(facelist, GL_COMPILE);
 		for (int i=0; i<polyNum; ++i)
 		{
 			glBegin(GL_POLYGON);
@@ -163,20 +167,20 @@ void init()
 				glVertex3f(vertCoordArr[vertIndx3*3], vertCoordArr[vertIndx3*3+1], vertCoordArr[vertIndx3*3+2]);
 			glEnd();
 		}
-	glEndList();
+	glEndList();*/
 	
-	/*
+	
 	// compile the display list
 	glNewList(facelist, GL_COMPILE);
 		glBegin(GL_POLYGON);
 			glColor3f(1,1,0);
-			glVertex3f(1,1,1);
+			glVertex3f(0.01,-0.01,0.01);
 			glColor3f(0,1,0);
-			glVertex3f(0,1,1);
+			glVertex3f(0,-0.1,0.01);
 			glColor3f(0,0,1);
-			glVertex3f(1,0,1);
+			glVertex3f(0.1,0,0.1);
 		glEnd();
-	glEndList();*/
+	glEndList();
 }
 
 /////////////////////////////////////////////////////////////////////////
@@ -237,7 +241,7 @@ void loadData()
 
 	// misc buffer variables for reading data, getline calls might resize both as needed
 	char *buf = NULL;
-	if (!(buf = (char*)malloc(1024))) { fprintf(stderr, "malloc failed\n"); exit(-1); }
+	if (!(buf = (char*) malloc(1024))) { fprintf(stderr, "malloc failed\n"); exit(-1); }
 	size_t bufsize = 1024;
 
 
@@ -253,9 +257,9 @@ void loadData()
 	fscanf(vtk_fdesc, "%s %ld %s", buf, &vertNum, buf);
 
 	// global vertex coordinate, normal (zeroed via calloc), texture arrays malloc
-	if (!(vertCoordArr = (GLfloat*)malloc(vertNum*3*sizeof(GLfloat)))) { fprintf(stderr, "malloc failed\n"); exit(-1); }
-	if (!(vertNormArr  = (GLfloat*)calloc((size_t)vertNum*3, sizeof(GLfloat)))) { fprintf(stderr, "calloc failed\n"); exit(-1); }
-	if (!(vertTexArr   = (GLfloat*)malloc(vertNum*2*sizeof(GLfloat)))) { fprintf(stderr, "malloc failed\n"); exit(-1); }
+	if (!(vertCoordArr = (GLfloat*) malloc(vertNum*3*sizeof(GLfloat)))) { fprintf(stderr, "malloc failed\n"); exit(-1); }
+	if (!(vertNormArr  = (GLfloat*) calloc((size_t)vertNum*3, sizeof(GLfloat)))) { fprintf(stderr, "calloc failed\n"); exit(-1); }
+	if (!(vertTexArr   = (GLfloat*) malloc(vertNum*2*sizeof(GLfloat)))) { fprintf(stderr, "malloc failed\n"); exit(-1); }
 
 	// parse data
 	for(int i=0; i<vertNum*3; ++i)
@@ -270,7 +274,7 @@ void loadData()
 	fscanf(vtk_fdesc, "%s %ld %ld", buf, &polyNum, &cellNum);
 
 	// global polygon array malloc
-	if (!(polyArr = (GLuint*)malloc(polyNum*3*sizeof(GLuint)))) { fprintf(stderr, "malloc failed\n"); exit(-1); }
+	if (!(polyArr = (GLuint*) malloc(polyNum*3*sizeof(GLuint)))) { fprintf(stderr, "malloc failed\n"); exit(-1); }
 		
 	// parse data
 	int polysize = 0; // NB: assumed to always be 3 in this exercise (as per data provided), varying poly sizes require dynamic arrays
@@ -283,24 +287,59 @@ void loadData()
 	}
 	
 
-	// Calculate and sum vertex normals
-	for (int i=0; i<polyNum*3; ++i)
-	{/*
+	// Calculate and sum vertex normalsi //TODO: borken 000001 000000110?
+	for (int i=0; i<polyNum; ++i)
+	{
 		GLfloat normal[3] = {0};
-		
-		GLuint vIdx = polyArr[i];
-
-		triangleNormal_unnormalised(vertCoordArr[vIdx*3],
-		vertCoordArr[vIdx*3+1],
-		vertCoordArr[vIdx*3+2],
-		normal);
-
-		//polyArr[i+1 +2]
-
-				glVertex3f(vertCoordArr[vIdx*3], vertCoordArr[vIdx*3+1], vertCoordArr[vIdx*3+2]);
-	//	printf("%u\n", polyArr[i]);
-	*/}
 	
+		// pick indexes of the three vertices making up the poly	
+		GLuint vertIndx1 = polyArr[i*3];
+		GLuint vertIndx2 = polyArr[i*3+1];
+		GLuint vertIndx3 = polyArr[i*3+2];
+		
+		// get normal (not normalised)
+		triangleNormal_unnormalised(&vertCoordArr[vertIndx1*3],&vertCoordArr[vertIndx2*3],&vertCoordArr[vertIndx3*3], normal);
+		
+		// add normal components to all relevant vertices (results in a unnormalised average of surface normals at vertices)
+		vertNormArr[vertIndx1*3] += normal[0];
+		vertNormArr[vertIndx1*3+1] += normal[1];
+		vertNormArr[vertIndx1*3+2] += normal[2];
+		
+		vertNormArr[vertIndx2*3] += normal[0];
+		vertNormArr[vertIndx2*3+1] += normal[1];
+		vertNormArr[vertIndx2*3+2] += normal[2];
+		
+		vertNormArr[vertIndx3*3] += normal[0];
+		vertNormArr[vertIndx3*3+1] += normal[1];
+		vertNormArr[vertIndx3*3+2] += normal[2];
+	}
+/*
+	// TODO: normalise all normals
+	for (int i=0; i<vertNum; i++)
+	{
+		int k = i*3;
+
+
+
+		printf("norm: %f ", vertNormArr[k]);
+	}*/
+	
+	/*for (int i=0; i<polyNum; ++i)
+	{
+		GLfloat tempres[3] = {0};
+		
+		GLuint vertIndx1 = polyArr[i*3];
+		GLuint vertIndx2 = polyArr[i*3+1];
+		GLuint vertIndx3 = polyArr[i*3+2];
+
+		printf("vertIndx: %d, %d, %d\n", vertIndx1, vertIndx2, vertIndx3);
+		printf("xyz for indx1: %f, %f, %f\n", vertCoordArr[vertIndx1*3], vertCoordArr[vertIndx1*3+1], vertCoordArr[vertIndx1*3+2]);
+		printf("xyz for indx2: %f, %f, %f\n", vertCoordArr[vertIndx2*3], vertCoordArr[vertIndx2*3+1], vertCoordArr[vertIndx2*3+2]);
+		printf("xyz for indx3: %f, %f, %f\n", vertCoordArr[vertIndx3*3], vertCoordArr[vertIndx3*3+1], vertCoordArr[vertIndx3*3+2]);
+
+		triangleNormal_unnormalised(&vertCoordArr[vertIndx1*3],&vertCoordArr[vertIndx2*3],&vertCoordArr[vertIndx3*3], tempres);
+		printf("normal %d returned: %f10, %f10, %f10\n\n", i, tempres[0],tempres[1],tempres[2]);
+	}*/
 	
 	// TODO:	TEXTURE DATA PROCESSING HERE
 	//getline(&buf, &bufsize, vtk_fdesc); 
@@ -354,6 +393,10 @@ void triangleNormal_unnormalised(GLfloat pt1[3], GLfloat pt2[3], GLfloat pt3[3],
 	v2[1] = pt1[1] - pt3[1];
 	v2[2] = pt1[2] - pt3[2];
 
+	// TODO: DEBUG printf
+	//printf("trigNormal first vector: (%f10, %f10, %f10)\n", v1[0], v1[1], v1[2]);
+	//printf("trigNormal second vector: (%f10, %f10, %f10)\n", v2[0], v2[1], v2[2]);
+	
 	// calculate cross product (writes over out)
 	cross3f(v1,v2,out);
 }
